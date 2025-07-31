0144b74969f8dec54035de0ca4f7330f0f3c5170
Oleksandr Kiyenko <o.kiyenko@gmail.com>
2025-07-31 08:47:43 +0200

    Add pcireg initialization

--- drivers/pci/pcie-xilinx-nwl.c
+++ drivers/pci/pcie-xilinx-nwl.c
@@ -311,6 +311,13 @@ static int nwl_pcie_parse_dt(struct nwl_pcie *pcie)
 		return PTR_ERR(pcie->ecam_base);
 	pcie->phys_ecam_base = res.start;
 
+	ret = dev_read_resource_byname(dev, "pcireg", &res);
+	if (ret)
+		return ret;
+	pcie->pcireg_base = devm_ioremap(dev, res.start, resource_size(&res));
+	if (IS_ERR(pcie->pcireg_base))
+		return PTR_ERR(pcie->pcireg_base);
+
 	return 0;
 }
 
